133c6ad4f772a5aad634dbcc81a1d3d4a78e7c95
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:21:23 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sixthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldBalance;
+            int countCrystalsToBuy;
+            int crystalPrice = 67;
+
+            Console.Write("Укажите ваш баланс золотых монет:\t");
+            goldBalance = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Вам отправлено предложение о покупке кристаллов");
+            Console.WriteLine("Цена за кристалл {0} золотых", crystalPrice);
+            Console.Write("Кристаллов к покупке:\t");
+
+            countCrystalsToBuy = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кристаллов куплено: {0}", countCrystalsToBuy);
+
+            goldBalance = goldBalance - (countCrystalsToBuy * crystalPrice);
+            Console.WriteLine("Ваш остаток золотых: {0}", goldBalance);
+        }
+    }
+}
